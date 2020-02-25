939794ffcb166be4afb395b8de5bba9380e80b3b
firvain <etsipis@terracognita.eu>
2020-02-25 10:32:17 +0200

    added package.json details

--- loaders/express.js
+++ loaders/express.js
@@ -7,6 +7,7 @@ const routes = require("../api");
 
 module.exports = async ({ app }) => {
   app.enable("trust proxy");
+  app.use(helmet());
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
@@ -17,7 +18,6 @@ module.exports = async ({ app }) => {
     })
   );
   app.use(morgan("dev"));
-  app.use(helmet());
   app.use(cors());
   app.use("/api", routes());
 };
--- loaders/mongoose.js
+++ loaders/mongoose.js
@@ -1,6 +1,4 @@
 const db = require("../db");
 module.exports = async () => {
-  // const a = await db();
-  // console.log(a);
   return await db();
 };
